8984b973153ea1bc72f9f4f552d5600be026af0e
Jhankar Bansal <bjhankar09@gmail.com>
2026-05-27 11:22:48 +0530

    i have talked about dotenc here

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const express=require("express")
+const express=require("express")     //reqi=uire module expxress  
 const app=express()
-const port=3000
+require('dotenv').config()
+const port=9000
 
+app.get('/',(req,res)=>
+{
+    res.send("ehllo g")
+})
+
+app.get('/youtubekabaap',(req,res)=>
+{
+    res.send("<h2>poori namak</h2>")
+})
+
+// app.listen(port, () =>
+// {
+//     console.log(`Example app listening on port ${port}`)
+
+// })
+
+app.listen(process.env.PORT, ()=>
+{
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
